ff2b59f2b1021128ab2f5319d47dd43de2bd8593
David Ashford <david@dashford.io>
2020-11-28 12:47:17 +0000

    extra logging for captured values

--- src/subscribers/wsdcgq12lm.py
+++ src/subscribers/wsdcgq12lm.py
@@ -32,26 +32,32 @@ class WSDCGQ12LM:
         battery_signal = signal('battery')
         self._logger.debug('Broadcasting battery signal')
         battery_signal.send(self, battery=self._get_battery())
+        self._logger.info('Broadcast battery value of {}'.format(self._get_battery()))
 
         humidity_signal = signal('humidity')
         self._logger.debug('Broadcasting humidity signal')
         humidity_signal.send(self, humidity=self._get_humidity())
+        self._logger.info('Broadcast humidity value of {}'.format(self._get_humidity()))
 
         link_quality_signal = signal('link_quality')
         self._logger.debug('Broadcasting link_quality signal')
         link_quality_signal.send(self, link_quality=self._get_link_quality())
+        self._logger.info('Broadcast link_quality value of {}'.format(self._get_link_quality()))
 
         pressure_signal = signal('pressure')
         self._logger.debug('Broadcasting pressure signal')
         pressure_signal.send(self, pressure=self._get_pressure())
+        self._logger.info('Broadcast pressure value of {}'.format(self._get_pressure()))
 
         temperature_signal = signal('temperature')
         self._logger.debug('Broadcasting temperature signal')
         temperature_signal.send(self, temperature=self._get_temperature())
+        self._logger.info('Broadcast temperature value of {}'.format(self._get_temperature()))
 
         voltage_signal = signal('voltage')
         self._logger.debug('Broadcasting voltage signal')
         voltage_signal.send(self, voltage=self._get_voltage())
+        self._logger.info('Broadcast voltage value of {}'.format(self._get_voltage()))
 
 
 # payload '{"battery":100,"humidity":70.92,"linkquality":5,"pressure":1019.3,"temperature":13.96,"voltage":3065}'
